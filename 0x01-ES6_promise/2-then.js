f28ab4b439ad7e2106faab6e73634aafc20107dd
Solomon <solomonuche@gmail.com>
2024-04-11 12:45:50 +0100

    Append three handlers to the function

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .finally(() => {
+      console.log('Got a response from the API');
+    })
+    .catch(() => new Error());
+}
