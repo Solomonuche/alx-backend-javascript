6a92b136bc8312fe5d23724b83bc5591e7efceb3
Solomon <solomonuche@gmail.com>
2024-04-12 01:18:55 +0100

    Implement a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super(currency.code, currency.name);
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
