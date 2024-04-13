5583b3d1ec2b659c25c60dc8a4c31baa462fdf2a
Solomon <solomonuche@gmail.com>
2024-04-13 12:07:59 +0100

    promise.allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => [
-      {
-        status: 'fulfilled',
-        value: result,
-      },
-    ])
-    .catch((error) => [
-      {
-        status: 'rejected',
-        value: error,
-      },
-    ]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      const obj = [];
+      result.forEach((res) => {
+        obj.push({
+          status: res.status,
+          value: res.status === 'fulfilled' ? res.value : String(res.reason),
+        });
+      });
+      return obj;
+    });
 }
