4c4471625a2cce1d29ba2d436e7b605d3817a7c9
Solomon <solomonuche@gmail.com>
2024-06-07 00:09:18 +0100

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,8 +4,17 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function () {
   it('Should return sum', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  }); 
+  
+  it('Should return sum', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  }); 
+
+  it('Should return sum', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  }); 
+
+  it('Should return sum', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });    
+  }); 
 });
