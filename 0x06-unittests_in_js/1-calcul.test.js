b3e6bfd111e0afa7ec22802ed381901b3dd98031
Solomon <solomonuche@gmail.com>
2024-06-07 01:36:26 +0100

    Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,49 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+const { it } = require('mocha');
+
+describe('calculateNumber', function () {
+  describe('test addition', function () {
+    it('testing with whole numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1, 3), 4);
+      }); 
+      
+      it('test with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1, 3.7), 5);
+      }); 
+    
+      it('test with both float numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1.2, 3.7), 5);
+      }); 
+  });
+
+  // Type substraction
+  describe('test substration', function () {
+    it('testing with whole numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 1, 3), -2);
+      }); 
+      
+      it('test with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 5, 3.7), 1);
+      }); 
+    
+      it('test with both float numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 5.2, 3.7), 1);
+      }); 
+  });
+
+  // Division
+  describe('test division', function () {
+    it('testing with whole numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 4, 2), 2);
+      }); 
+      
+      it('test with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 8, 3.7), 2);
+      }); 
+    
+      it('test with both float numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 4.2, 0), "Error");
+      }); 
+  });
+});
