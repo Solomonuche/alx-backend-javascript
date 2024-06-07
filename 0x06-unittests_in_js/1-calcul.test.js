5d5e114fcfa8730b1676de47459c8eaa67073e88
Solomon <solomonuche@gmail.com>
2024-06-07 02:37:43 +0100

    test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,29 +1,48 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
-describe('test for the calculator', () => {
-  describe('test SUM function', () => {
-    it('should round of arguments and return sum', () => {
-      const sum1 = calculateNumber('SUM', 1.4, 4.5);
-      const sum2 = calculateNumber('SUM', 2, 3.1);
-      assert.strictEqual(sum1, 6);
-      assert.strictEqual(sum2, 5);
-    });
+describe('Test for basic calculator', function () {
+  describe('test addition', function () {
+    it('testing with whole numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1, 3), 4);
+      }); 
+      
+      it('test with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1, 3.7), 5);
+      }); 
+    
+      it('test with both float numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1.2, 3.7), 5);
+      }); 
   });
-  describe('test SUBTRACT function', () => {
-    it('should round of arguments and return the difference', () => {
-      const diff1 = calculateNumber('SUBTRACT', 7.3, 0.3);
-      const diff2 = calculateNumber('SUBTRACT', 5.0, 9);
-      assert.strictEqual(diff1, 7);
-      assert.strictEqual(diff2, -4);
-    });
+
+  // Type substraction
+  describe('test substration', function () {
+    it('testing with whole numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 1, 3), -2);
+      }); 
+      
+      it('test with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 5, 3.7), 1);
+      }); 
+    
+      it('test with both float numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 5.2, 3.7), 1);
+      }); 
   });
-  describe('test DIVIDE function', () => {
-    it('should round of arguments and return the difference', () => {
-      const quo1 = calculateNumber('DIVIDE', -10, 2);
-      const quo2 = calculateNumber('DIVIDE', 5.0, 0);
-      assert.strictEqual(quo1, -5);
-      assert.strictEqual(quo2, 'Error');
-    });
+
+  // Division
+  describe('test division', function () {
+    it('testing with whole numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 4, 2), 2);
+      }); 
+      
+      it('test with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 8, 3.7), 2);
+      }); 
+    
+      it('test with both float numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 4.2, 0), "Error");
+      }); 
   });
 });
