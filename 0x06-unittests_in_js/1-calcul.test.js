86ce4a3b27960fc45c59fed0264a00d7c7950342
Solomon <solomonuche@gmail.com>
2024-06-07 02:45:39 +0100

    test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,46 +3,46 @@ const calculateNumber = require('./1-calcul');
 
 describe('Test for basic calculator', function () {
   describe('test addition', function () {
-    it('testing with whole numbers', function () {
+    it('with whole numbers', function () {
         assert.strictEqual(calculateNumber("SUM", 1, 3), 4);
       }); 
       
-      it('test with whole and float numbers', function () {
+      it('with whole and float numbers', function () {
         assert.strictEqual(calculateNumber("SUM", 1, 3.7), 5);
       }); 
     
-      it('test with both float numbers', function () {
-        assert.strictEqual(calculateNumber("SUM", 1.2, 3.7), 5);
+      it('with both float numbers', function () {
+        assert.strictEqual(calculateNumber("SUM", 1.4, 4.5), 6);
       }); 
   });
 
   // Type substraction
   describe('test substration', function () {
-    it('testing with whole numbers', function () {
+    it('teswith whole numbers', function () {
         assert.strictEqual(calculateNumber("SUBSTRACT", 1, 3), -2);
       }); 
       
-      it('test with whole and float numbers', function () {
+      it('with whole and float numbers', function () {
         assert.strictEqual(calculateNumber("SUBSTRACT", 5, 3.7), 1);
       }); 
     
-      it('test with both float numbers', function () {
-        assert.strictEqual(calculateNumber("SUBSTRACT", 5.2, 3.7), 1);
+      it('with both float numbers', function () {
+        assert.strictEqual(calculateNumber("SUBSTRACT", 1.4, 4.5), -4);
       }); 
   });
 
   // Division
   describe('test division', function () {
-    it('testing with whole numbers', function () {
+    it('teswith whole numbers', function () {
         assert.strictEqual(calculateNumber("DIVIDE", 4, 2), 2);
       }); 
       
-      it('test with whole and float numbers', function () {
-        assert.strictEqual(calculateNumber("DIVIDE", 8, 3.7), 2);
+      it('with whole and float numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
       }); 
     
-      it('test with both float numbers', function () {
-        assert.strictEqual(calculateNumber("DIVIDE", 4.2, 0), "Error");
+      it('with both float numbers', function () {
+        assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0), "Error");
       }); 
   });
 });
